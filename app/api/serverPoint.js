a541184418288a7e68c8edfd6848d02133f0daaa
aliknd <a.kargaran.d@gmail.com>
2023-10-24 23:08:28 -1000

    setting the new url for github

--- app/api/serverPoint.js
+++ app/api/serverPoint.js
@@ -1,3 +1,3 @@
 //const endpointURL = "https://banaware.hawaiidigitalhealthlab.com/api";
-const endpointURL = "http://168.105.244.46:9000/api";
+const endpointURL = "http://192.168.86.30:9000/api";
 export default endpointURL;
